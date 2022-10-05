1369e6fce2f58cae4f0a3fd34826a9d9e8e7621e
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-05 18:03:45 +0200

    Add routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,31 @@
 import express from "express";
 import cors from "cors";
+import { getUserInfo, getUserGalleries } from "../db/query.js";
 
 const router = express.Router();
 
-//middleware
+// middleware
 router.use(express.json());
-router.use(cors())
+router.use(cors());
 
+// get User Information
+router.get("/:id", async (req, res) => {
+  try {
+    const responseDb = await getUserInfo(req.params.id);
+    res.status(responseDb.status).json(responseDb.result);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
-router.get("/", async (req, res) => {
-  res.send("Welcome to VR-Gallery-API");
+// get all Galleries owned by a User 
+router.get("/galleries/:id", async (req, res) => {
+  try {
+    const responseDb = await getUserGalleries(req.params.id);
+    res.status(responseDb.status).json(responseDb.result);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 export default router;
