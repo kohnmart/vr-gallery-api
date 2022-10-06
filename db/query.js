8e5870186368a8a9a88bf1eca1d8ae3ec710493c
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-06 17:56:29 +0200

    Update queries

--- db/query.js
+++ db/query.js
@@ -1,14 +1,29 @@
-import pool from "./db.js";
+import query from "./db.js";
 
+const getUserInfo = async (user_id) => {
+  const result = await query(
+    "SELECT * FROM public.user WHERE public.user.user_id = $1",
+    [user_id]
+  );
+  return { status: 200, result: result.rows };
+};
 
-export const getUserInfo = async (user_id) => {
-    const result = await pool.query("SELECT * FROM public.user WHERE public.user.user_id = $1", [user_id]);
-    return { status: 200, result: result.rows };
-  };
+const getUserGalleries = async (user_id) => {
+  const result = await query(
+    `SELECT
+        g_name,
+        g_date,
+        g_rating,
+        g_active,
+        g_path
+    FROM
+        public."getUserGalleries"
+    WHERE
+        u_id = $1;`,
+    [user_id]
+  );
+  return { status: 200, result: result.rows };
+};
 
-  export const getUserGalleries = async (user_id) => {
-    const result = await pool.query(
-    "SELECT * FROM public.user JOIN public.gallery ON public.gallery.u_id = public.user.user_id WHERE public.user.user_id = $1", [user_id]);
-    return { status: 200, result: result.rows };
-  }
-
+export { getUserInfo, getUserGalleries }
+export default { getUserInfo, getUserGalleries }
