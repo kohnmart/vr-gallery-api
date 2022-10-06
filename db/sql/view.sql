3cb085db49387e1c7c3ac424c65e770b952b5165
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-06 17:57:21 +0200

    Add view file

---
+++ db/sql/view.sql
@@ -0,0 +1,29 @@
+/* getUserGalleryView */
+
+    /* Create */
+    SELECT
+        gallery.u_id,
+        gallery.g_name,
+        gallery.g_date,
+        gallery.g_rating,
+        gallery.g_active,
+        gallery.g_path
+    FROM
+        gallery
+        JOIN "user" ON "user".u_id = gallery.u_id
+    WHERE
+        "user".u_id = gallery.u_id;
+
+    /* API Select */
+    SELECT
+        g_name,
+        g_date,
+        g_rating,
+        g_active,
+        g_path
+    FROM
+        public."getUserGalleries"
+    WHERE
+        u_id = '68866309-361c-445b-a7d6-0093bbea0c14';
+
+/**********************/
