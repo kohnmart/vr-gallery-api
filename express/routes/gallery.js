5c9b665d60167b2f5ab6a3a56c09c799a81d9f13
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-07 15:50:45 +0200

    Update for using requestView

--- express/routes/gallery.js
+++ express/routes/gallery.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { getUserGalleries } from "../../postgres-db/query.js";
-
+import requestView from "../../postgres-db/request.js";
 const galleries = express.Router();
 
 // middleware
@@ -9,12 +8,17 @@ galleries.use(express.json());
 // get all Galleries owned by a User
 galleries.get("/:id", async (req, res) => {
   try {
-    const responseDb = await getUserGalleries(req.params.id);
-    res.status(responseDb.status).json(responseDb.result);
+    const db = await requestView({
+      selects: ["g_name", "g_date", "g_rating", "g_active", "g_path"],
+      view: "getUserGalleries",
+      idName: "u_id",
+      idVal: req.params.id,
+    });
+    res.status(db.status).json(db.result);
   } catch (err) {
     console.log(err.message);
   }
 });
 
-export { galleries as router };
+export { galleries };
 export default galleries;
