00d14311c0f950b7418bd99b873d05ddd0813de3
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-08 17:57:59 +0200

    Update gallery routes

--- express/routes/gallery.js
+++ express/routes/gallery.js
@@ -1,18 +1,17 @@
 import express from "express";
-import requestView, { insertTable } from "../../postgres-db/request.js";
+import actionDatabase from "../../postgres-db/request.js";
 const galleries = express.Router();
-
-// middleware
 galleries.use(express.json());
 
-// get all Galleries owned by a User
-galleries.get("/:id", async (req, res) => {
+// GET ALL ACTIVE GALLERIES
+galleries.get("/", async (req, res) => {
   try {
-    const db = await requestView({
-      selects: ["g_name", "g_date", "g_rating", "g_active", "g_path"],
-      view: "getUserGalleries",
-      idName: "u_id",
-      idVal: req.params.id,
+    const db = await actionDatabase({
+      method: "select",
+      select: ["g_name", "g_date", "g_rating", "g_active", "g_path"],
+      table: "getUserGalleries",
+      idName: "g_active",
+      idValue: "true",
     });
     res.status(db.status).json(db.result);
   } catch (err) {
@@ -20,19 +19,68 @@ galleries.get("/:id", async (req, res) => {
   }
 });
 
+// GET ALL USER GALLERIES
+galleries.get("/:id", async (req, res) => {
+  try {
+    const db = await actionDatabase({
+      method: "select",
+      select: ["g_name", "g_date", "g_rating", "g_active", "g_path"],
+      table: "getUserGalleries",
+      idName: "u_id",
+      idValue: req.params.id,
+    });
+    res.status(db.status).json(db.result);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// CREATE NEW GALLERY
 galleries.post("/", async (req, res) => {
   const post = req.body;
   try {
-    const db = await insertTable({
+    const db = await actionDatabase({
+      method: "insert",
       table: "gallery",
-      col: ["u_id", "g_name", "g_active", "g_path"],
-      val: [post.id, post.name, post.active, post.path],
+      columns: ["u_id", "g_name", "g_active", "g_path"],
+      set : [post.id, post.name, post.active, post.path],
     });
     res.status(db.status).json(db.result);
   } catch (err) {
     console.log(err.message);
   }
 });
+
+// UPDATE GALLERY COLUMN 
+galleries.put("/", async (req, res) => {
+  try {
+    const db = await actionDatabase({
+      method: "update",
+      table: "gallery",
+      columns: "g_active",
+      idName: ["g_id", "u_id"],
+      idValue:  req.body.ids,
+      set: req.body.set
+    });
+    res.status(db.status).json(db.result);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+galleries.delete("/", async (req, res) => {
+  try {
+    const db = await actionDatabase({
+      method: "delete",
+      table: "gallery",
+      idName: ["g_id", "u_id"],
+      idValue: [req.query.g_id, req.query.u_id]
+    });
+    res.status(db.status).json(db.result);
+  } catch (err) {
+    console.log(err.message);
+  }
+})
 
 export { galleries };
 export default galleries;
