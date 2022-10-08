eb502ea7355e01078807abae7183a5ded62f4586
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-08 17:58:51 +0200

    Update request to use a single method with cases

--- postgres-db/request.js
+++ postgres-db/request.js
@@ -1,38 +1,55 @@
 import query from "./db.js";
+import { concatStatement, quoteValues } from "./helper.js";
 
-const requestView = async (obj) => {
-  const select = obj.selects.join(", ");
-  const view = obj.view;
-  const idName = obj.idName;
-  const idVal = obj.idVal;
-
-  // console.log(`SELECT ${select} FROM "${view}" WHERE ${idName} = ${idVal};`);
-
-  const result = await query(
-    `SELECT
-        ${select}
-    FROM
-       "${view}"
-    WHERE
-        ${idName} = $1;`,
-    [idVal]
-  );
-
-  return { status: 200, result: result.rows };
-};
-
-const insertTable = async (obj) => {
+const actionDatabase = async (obj) => {
+  const method = obj.method;
+  const select = obj.select;
+  const columns = obj.columns;
   const table = obj.table;
-  const col = obj.col;
-  const val = [];
-   obj.val.forEach(element => {
-     val.push("'" + element + "'");
-  });
+  const idName = obj.idName;
+  const idValue = obj.idValue;
+  const set = obj.set;
 
-  const result = await query(`INSERT INTO ${table}(${col}) VALUES(${val})`);
+  let queryString;
+
+  switch (method) {
+    case "select":
+      queryString = `SELECT ${select} FROM "${table}" `;
+
+      const constraint = `${
+        idName ? "WHERE " + idName + "=" + "'" + idValue + "'" : ""
+      }`;
+
+      constraint ? (queryString += constraint) : queryString;
+
+      break;
+
+    case "insert":
+      queryString = `INSERT INTO ${table}(${columns}) VALUES(${quoteValues(
+        set
+      )})`;
+
+      break;
+
+    case "update":
+      queryString = `UPDATE "${table}" SET ${columns} = ${set} WHERE ${concatStatement(
+        idName,
+        quoteValues(idValue)
+      )}`;
+
+      break;
+
+    case "delete":
+      queryString = `DELETE FROM "${table}" WHERE  ${concatStatement(
+        idName,
+        quoteValues(idValue)
+      )}`;
+        console.log(queryString);
+      break;
+  }
+  const result = await query(queryString);
 
   return { status: 200, result: result.rows };
 };
 
-export { requestView, insertTable };
-export default { requestView, insertTable };
+export default actionDatabase;
