06d5eb3387de9db19e2d1bffa89fedc7321cf42e
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-09 14:22:55 +0200

    Update user routes

--- express/routes/user.js
+++ express/routes/user.js
@@ -1,9 +1,39 @@
+import express from "express";
+import actionDatabase from "../../postgres-db/request.js";
+const user = express.Router();
+user.use(express.json());
+
+
 // get User Information
-router.get("/:id", async (req, res) => {
+user.get("/:id", async (req, res) => {
   try {
-    //  const responseDb = await getUserInfo(req.params.id);
-    res.status(responseDb.status).json(responseDb.result);
+    const db = await actionDatabase({
+      method: "select",
+      select: ["u_id", "u_name"],
+      table: "user",
+      idName: "u_id",
+      idValue: req.params.id,
+    });
+    res.status(db.status).json(db.result);
   } catch (err) {
     console.log(err.message);
   }
 });
+
+// Create new User
+user.post("/", async (req, res) => {
+  try {
+    const db = await actionDatabase({
+      method: "insert",
+      table: "user",
+      columns: ["u_name"],
+      set: [req.body.name],
+      returningId: "u_id"
+    });
+    res.status(db.status).json(db.result);
+  } catch (err) {
+    console.log(err.message);
+  }
+})
+
+export default user;
