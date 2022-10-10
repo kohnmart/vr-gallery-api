510b77781e6d3c57010ff64664a48413c97aa098
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-10 17:59:32 +0200

    Add image route

--- express/index.js
+++ express/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import galleries from "./routes/gallery.js";
 import user from "./routes/user.js";
+import image from "./routes/image.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use("/api/galleries/", galleries);
 app.use("/api/user/", user);
+app.use("/api/image", image);
 const port = 5000;
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
