1625e67db2c669f13127932bb1a12f7561862a31
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-10 17:59:55 +0200

    Update multer to store temporarily

--- express/middleware/upload.js
+++ express/middleware/upload.js
@@ -1,16 +1,15 @@
-import multer from 'multer';
-import path from 'path';
+import multer from "multer";
+import path from "path";
 
 //Storage
 const storage = multer.diskStorage({
-    destination: './store/',
-    filename: function (req, file, cb) 
-    {
-        cb(null, file.filename + path.extname(file.originalname));
-    }
+  destination: "./store/",
+  filename: function (req, file, cb) {
+    cb(null, file.filename + path.extname(file.originalname));
+  }
 });
 
-// Init 
-const upload = multer({ storage: storage });
+// Create multer instance middlware
+const upload = multer({ dest: "store/" }).single("image");
 
 export default upload;
