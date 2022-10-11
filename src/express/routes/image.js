b985516555506f5178059a31bab0f06b8faf734e
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-11 18:10:29 +0200

    Add upload image route

--- src/express/routes/image.js
+++ src/express/routes/image.js
@@ -4,28 +4,33 @@ import actionDatabase from "../../postgres-db/request.js";
 const image = express.Router();
 image.use(express.json());
 
-// GET ALL GALLERY IMAGES
+// GET ALL GALLERY IMAGES FROM A USER
 image.get("/", async (req, res) => {
-  try {
-    const db = await actionDatabase({
-      method: "select",
-      select: ["i_id", "i_name", "g_id", "g_name"],
-      table: "getGalleryImages",
-      idName: "g_id",
-      idValue: req.params.id,
-    });
-    res.status(db.status).json(db.result);
-  } catch (err) {
-    console.log(err.message);
-  }
+  const db = await actionDatabase({
+    method: "select",
+    select: ["i_id", "i_name", "g_id", "g_name"],
+    table: "getGalleryImages",
+    idName: "g_id",
+    idValue: req.params.id,
+  });
+  res.status(db.status).json(db.result);
 });
-
 
 // ADD IMAGE TO GALLERY --> Upload to S3 Bucket
 image.post("/upload", upload, async (req, res) => {
-  const file = req.file;
-  const id = req.body.id;
-  res.send(id);
+  const image = req.file;
+  const imageId = image.filename.split('.')[0];
+  const g_id = req.body.g_id;
+
+  console.log(image);  
+  const db = await actionDatabase({
+    method: "insert",
+    table: "image",
+    columns: ["i_id", "g_id", "i_name"],
+    set: [imageId, g_id, image.originalname],
+    returningId: "i_id"
+  });
+  res.status(db.status).json(db.result);
 });
 
 export default image;
