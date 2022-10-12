01b319e6d65f7c2403d83c9ea5df7d068e018451
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-12 17:13:31 +0200

    Add comments and formatting

--- src/express/index.js
+++ src/express/index.js
@@ -1,15 +1,31 @@
+//Import
 import express from "express";
+import session from "express-session"
 import galleries from "./routes/gallery.js";
 import user from "./routes/user.js";
 import image from "./routes/image.js";
+import auth from "./routes/auth.js"
 import dotenv from "dotenv";
+import passport from "passport";
 dotenv.config();
 
+
+
+// Server
 const app = express();
 
+//Session
+app.use(session( { secret: process.env.SESSION_SECRET}));
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//ROUTES
 app.use("/api/galleries", galleries);
 app.use("/api/user", user);
 app.use("/api/image", image);
+app.use("/api/auth", auth);
 app.use("/api/store", express.static("./store"));
 
 app.listen(process.env.PORT, () => console.log(`Listening to port ${process.env.PORT}`));
