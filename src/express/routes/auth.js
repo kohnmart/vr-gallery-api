049960e9dafd5b8b74330fe004de61ec32b02c3d
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-12 19:06:11 +0200

    Update auth route

--- src/express/routes/auth.js
+++ src/express/routes/auth.js
@@ -1,27 +1,29 @@
 import express from "express";
-import actionDatabase from "../../postgres-db/sql/helper/request.js";
 import "../middlewares/auth/google-config.js";
 import passport from "passport";
+import checkLogin from "../middlewares/auth/login.js";
 const auth = express.Router();
 
 // get User Information
-auth.get("/google", 
-passport.authenticate('google', { scope: ["email", "profile"]}));
+auth.get(
+  "/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
 
-auth.get('/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/api/auth/failure' }),
-  function(req, res) {
-    console.log(res);
-    res.redirect('/api/auth/success');
-  });
+auth.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/api/auth/failure" }),
+  function (req, res) {
+    res.redirect("/api/auth/success");
+  }
+);
 
-auth.get('/success', (req, res) => {
-    res.send("Successfully logged in!");
+auth.get("/success", checkLogin, (req, res) => {
+  res.send("Successfully logged in!");
 });
 
-auth.get('/failure', (req, res) => {
-    res.send("Login failed!");
+auth.get("/failure", (req, res) => {
+  res.send("Login failed!");
 });
-
 
 export default auth;
