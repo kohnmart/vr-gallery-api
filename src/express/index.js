30a4dcf24d9e59b06f7ed5cd3b3312d40ddf1695
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-13 17:44:00 +0200

    Update session config

--- src/express/index.js
+++ src/express/index.js
@@ -1,21 +1,25 @@
 //Import
 import express from "express";
-import session from "express-session"
+import session from "express-session";
 import galleries from "./routes/gallery.js";
 import user from "./routes/user.js";
 import image from "./routes/image.js";
-import auth from "./routes/auth.js"
+import auth from "./routes/auth.js";
 import dotenv from "dotenv";
 import passport from "passport";
 dotenv.config();
-
-
 
 // Server
 const app = express();
 
 //Session
-app.use(session( { secret: process.env.SESSION_SECRET}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
 
 //Passport
 app.use(passport.initialize());
@@ -28,4 +32,6 @@ app.use("/api/image", image);
 app.use("/api/auth", auth);
 app.use("/api/store", express.static("./store"));
 
-app.listen(process.env.PORT, () => console.log(`Listening to port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Listening to port ${process.env.PORT}`)
+);
