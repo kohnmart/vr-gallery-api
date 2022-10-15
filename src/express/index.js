05e170eca0bfde8b4ef978e5d84ddc443126740a
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-15 14:51:11 +0200

    Update single quite

--- src/express/index.js
+++ src/express/index.js
@@ -1,13 +1,14 @@
 //Import
-import express from "express";
-import session from "express-session";
-import galleries from "./routes/gallery.js";
-import user from "./routes/user.js";
-import image from "./routes/image.js";
-import auth from "./routes/auth.js";
-import dotenv from "dotenv";
-import passport from "passport";
-import checkLogin from "./middlewares/auth/login.js";
+import express from 'express';
+import session from 'express-session';
+import galleries from './routes/gallery.js';
+import user from './routes/user.js';
+import image from './routes/image.js';
+import auth from './routes/auth.js';
+import dotenv from 'dotenv';
+import passport from 'passport';
+import checkLogin from './middlewares/auth/login.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 // Server
@@ -16,10 +17,11 @@ const app = express();
 //Session
 app.use(
   session({
+    name: 'vr-gallery-cookie',
     secret: process.env.SESSION_SECRET,
     saveUninitialized: false,
     resave: false,
-    cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 } // 1 day
+    cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 }, // 1 day
   })
 );
 
@@ -27,14 +29,15 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(cookieParser());
 
 //OPEN ROUTE FOR AUTH
-app.use("/api/auth", auth);
+app.use('/api/auth', auth);
 //PROTECTED ROUTES
-app.use("/api/galleries", checkLogin, galleries);
-app.use("/api/user",      checkLogin, user);
-app.use("/api/image",     checkLogin, image);
-app.use("/api/store",     checkLogin, express.static("./store"));
+app.use('/api/galleries', checkLogin, galleries);
+app.use('/api/user', checkLogin, user);
+app.use('/api/image', checkLogin, image);
+app.use('/api/store', checkLogin, express.static('./store'));
 
 app.listen(process.env.PORT, () =>
   console.log(`Listening to port ${process.env.PORT}`)
