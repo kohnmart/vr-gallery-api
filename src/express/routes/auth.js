35d39bff0d33bffb596b9aec9c702cb2226cdb6e
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-15 14:51:22 +0200

    Update single quote

--- src/express/routes/auth.js
+++ src/express/routes/auth.js
@@ -1,37 +1,38 @@
-import express from "express";
-import "../middlewares/auth/google-strategy.js";
-import passport from "passport";
-import checkLogin from "../middlewares/auth/login.js";
+import express from 'express';
+import '../middlewares/auth/google-strategy.js';
+import passport from 'passport';
+import checkLogin from '../middlewares/auth/login.js';
 const auth = express.Router();
 
 // get User Information
 auth.get(
-  "/google",
-  passport.authenticate("google", { scope: ["email", "profile"] })
+  '/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] })
 );
 
 auth.get(
-  "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/api/auth/failure" }),
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/api/auth/failure' }),
   function (req, res) {
-    console.log("REQUEST");
+    console.log('REQUEST');
     console.log(req.user);
-    res.redirect("/api/auth/success");
+    res.redirect('/api/auth/success');
   }
 );
 
-auth.get("/success", checkLogin, (req, res) => {
-  res.send("Successfully logged in!");
+auth.get('/success', checkLogin, (req, res) => {
+  console.log(req.headers);
+  res.send('Successfully logged in!');
 });
 
-auth.get("/failure", (req, res) => {
-  res.send("Login failed!");
+auth.get('/failure', (req, res) => {
+  res.send('Login failed!');
 });
 
-auth.post("/logout", (req, res) => {
+auth.post('/logout', (req, res) => {
   req.logout();
-  res.send("User logged out");
-  console.log("User logged out!");
+  res.send('User logged out');
+  console.log('User logged out!');
 });
 
 export default auth;
