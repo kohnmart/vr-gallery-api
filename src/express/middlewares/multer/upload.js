cd996e64c3b43e5246373e0bb4c9ace171a6c083
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-17 21:09:45 +0200

    Upldate multer config for multiple images

--- src/express/middlewares/multer/upload.js
+++ src/express/middlewares/multer/upload.js
@@ -1,15 +1,16 @@
-import multer from "multer";
-import path from "path";
-import { v4 as uuidv4 } from "uuid";
-//Storage
+import multer from 'multer';
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
+//Storage config
 const storage = multer.diskStorage({
-  destination: "./store",
+  destination: './store',
   filename: function (req, file, cb) {
     cb(null, uuidv4() + path.extname(file.originalname));
-  }
+  },
 });
 
 // Create multer instance middleware
-const upload = multer({ storage: storage }).single("image");
+const upload = multer({ storage: storage });
 
 export default upload;
