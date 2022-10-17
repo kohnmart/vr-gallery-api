da29ed0b43019fb1baee6819df169b0d73224c02
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-17 21:10:22 +0200

    Fix request methods syntax

--- src/postgres-db/helper/request.js
+++ src/postgres-db/helper/request.js
@@ -1,5 +1,5 @@
-import query from "../config/db.js";
-import { concatStatement, quoteValues } from "./helper.js";
+import query from '../config/db.js';
+import { concatStatement, quoteValues } from './helper.js';
 
 const actionDatabase = async (obj) => {
   const method = obj.method;
@@ -14,33 +14,39 @@ const actionDatabase = async (obj) => {
   let queryString;
 
   switch (method) {
-    case "select":
+    case 'select':
       queryString = `SELECT ${select} FROM "${table}" `;
 
-      const constraint = `${
+      if (idName) {
+        const constraint = `WHERE ${concatStatement(
+          idName,
+          quoteValues(idValue)
+        )}`;
+        queryString += constraint;
+      }
+
+      /*  const constraint = `${
         idName ? "WHERE " + idName + "=" + "'" + idValue + "'" : ""
       }`;
 
       constraint ? (queryString += constraint) : queryString;
-
+      */
       break;
 
-    case "insert":
+    case 'insert':
       queryString = `INSERT INTO "${table}" (${columns}) VALUES(${quoteValues(
         set
       )}) RETURNING "${returningId}"`;
 
       break;
 
-    case "update":
-      queryString = `UPDATE "${table}" SET ${columns} = ${set} WHERE ${concatStatement(
-        idName,
-        quoteValues(idValue)
-      )}`;
+    case 'update':
+      queryString = `UPDATE "${table}" SET (${columns}) = (${quoteValues(set)})
+      WHERE ${concatStatement(idName, quoteValues(idValue))}`;
 
       break;
 
-    case "delete":
+    case 'delete':
       queryString = `DELETE FROM "${table}" WHERE  ${concatStatement(
         idName,
         quoteValues(idValue)
@@ -48,17 +54,17 @@ const actionDatabase = async (obj) => {
       break;
   }
   console.log(queryString);
-  
+
   //Execute concatenated query
   const result = query(queryString)
     .then((result) => {
       return { status: 200, result: result.rows };
     })
     .catch((err) => {
-      console.log(err);
+      console.log('ERROR DATABASE: ' + err);
       return { status: 400, result: err };
     });
-    
+
   return result;
 };
 
