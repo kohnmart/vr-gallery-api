8825ab72522f4cab9afdc42df28284b1e7ce39ae
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-10-18 11:05:17 +0200

    Add jwt-strategy

---
+++ src/express/middlewares/auth/jwt-strategy.js
@@ -0,0 +1,23 @@
+import { Strategy as JwtStrategy } from 'passport-jwt';
+import { ExtractJwt } from 'passport-jwt';
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.key = 'secretkey';
+opts.audience = 'localhost:5000';
+
+passport.use(
+  new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload.sub }, function (err, user) {
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+        // or you could create a new account
+      }
+    });
+  })
+);
